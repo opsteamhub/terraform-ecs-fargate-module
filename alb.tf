24ae9d180c7cfbdf6470420f799a009ee21fb613
rafael <rafaelviniciuscardoso@gmail.com>
2022-03-08 16:31:35 -0300

    update name

--- alb.tf
+++ alb.tf
@@ -1,5 +1,5 @@
 resource "aws_alb" "main" {
-  name            = replace(var.environment, var.name, "ecs-", "")
+  name            = replace(${var.environment}-${var.name}, "ecs-", "")
   subnets         = var.subnets
   security_groups = [aws_security_group.lb.id]
 
