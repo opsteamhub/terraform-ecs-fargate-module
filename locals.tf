b735a1b2a4663c43ac19855a538ab30e4f97c58f
rafael <rafaelviniciuscardoso@gmail.com>
2022-03-11 17:36:56 -0300

    add ecr_image

--- locals.tf
+++ locals.tf
@@ -1,4 +1,5 @@
 locals {
   log_name   = join("-", ["/ecs/task-definition", var.environment, var.name])
   stack_name = join("-", [var.environment, var.name])
+  aws_image  = "${var.aws_account}.dkr.ecr.${var.region}.amazonaws.com/${var.ecr_image}"
 }
